08c3ef3cc8219fff01d710a1c668ff3fa5458bd2
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2024-10-04 15:18:58 +0900

    [Gold IV] Title: 공유기 설치, Time: 256 ms, Memory: 28984 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    int n,c,answer,min,max;
+    int[] x;
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    int check(int dis){
+        int prev=x[0],cnt=1;
+        for(int i=1;i<n;i++){
+            if(x[i]-prev>=dis){
+                prev=x[i];
+                cnt++;
+            }
+        }
+        //System.out.println("cnt: "+cnt);
+        return cnt;
+    }
+
+    void solution() throws Exception{
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        x=new int[n];
+        for(int i=0;i<n;i++){
+            x[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(x);
+        min=x[0];
+        max=x[n-1];
+
+        int lt=1,rt=max-min+1;
+        //System.out.println("lt: "+lt+" rt: "+ rt);
+        while(lt<=rt){
+            int mid=(lt+rt)/2;
+            //System.out.println("mid: "+mid);
+            if(check(mid)<c){
+                rt=mid-1;
+            }
+            else{
+                answer=Math.max(answer,mid);
+                lt=mid+1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
