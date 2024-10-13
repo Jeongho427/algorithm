3254b72a6d66ee68264fff384c6b3a3a60817dba
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2024-10-13 14:47:37 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18180 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,44 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    int n,answer;
+    int[] dp;
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    void solution() throws Exception{
+        n = Integer.parseInt(br.readLine());
+        dp=new int[n+1];
+        for(int i=1;i<=n;i++){
+            dp[i]=Integer.MAX_VALUE;
+        }
+
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            if(i%3==0&&i%2!=0){
+                int num=dp[i/3]+1;
+                dp[i]=Math.min(dp[i],num);
+            }
+            if(i%2==0&&i%3!=0){
+                int num=dp[i/2]+1;
+                dp[i]=Math.min(dp[i],num);
+            }
+            if(i%3==0&&i%2==0){
+                int num=Math.min(dp[i/3],dp[i/2])+1;
+                dp[i]=Math.min(dp[i],num);
+            }
+            dp[i]=Math.min(dp[i],dp[i-1]+1);
+        }
+        answer=dp[n];
+
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
