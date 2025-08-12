38203d5f70a61e0844e2e28e6984e603e24090b7
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2025-08-12 14:36:03 +0900

    [Silver III] Title: 주유소, Time: 304 ms, Memory: 30480 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,48 @@
+
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+    int n,answer,distSum,curCost;
+    int[] dist;
+    int[] oil;
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    void solution() throws Exception {
+        n = Integer.parseInt(br.readLine());
+        dist = new int[n-1];
+        oil = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n-1;i++){
+            dist[i] = Integer.parseInt(st.nextToken());
+            distSum += dist[i];
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            oil[i] = Integer.parseInt(st.nextToken());
+        }
+
+        answer += oil[0]*dist[0];
+        distSum -= 2;
+        curCost = oil[0];
+        for(int i=1;i<n-1;i++){
+            if(curCost*distSum>oil[i]*distSum){
+                curCost = oil[i];
+            }
+            answer += curCost * dist[i];
+            distSum -= dist[i];
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
