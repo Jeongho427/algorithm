a3037305dde9b21254021fcfbb89480ca728394a
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2025-08-26 23:36:07 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,44 +1,29 @@
 import java.util.*;
 
 class Solution {
-    int L;
-    int cnt;
-    
+    int answer;
+    int l;
     boolean[] visit;
     int[][] map;
     
-    void DFS(int v){
-        if(!visit[v]){
-            cnt++;
-            visit[v]=true;
-        }
-        
-        if(v==L){
-            return;
-        }
-        
-        for(int i=0;i<=L;i++){
-            if(map[v][i]==1&&!visit[i]){
-                DFS(i);
+    void dfs(int x){
+        visit[x] = true;
+        for(int i=0;i<l;i++){
+            if(x!=i&&!visit[i]&&map[x][i]==1){
+                visit[i] = true;
+                dfs(i);
             }
         }
     }
     
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        L=n;
-        visit=new boolean[n+1];
-        map=new int[n+1][n+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                map[i][j]=computers[i][j];
-            }
-        }
+        l = n;
+        visit = new boolean[n];
+        map = computers;
         
         for(int i=0;i<n;i++){
-            cnt=0;
-            DFS(i);
-            if(cnt!=0){
+            if(!visit[i]){
+                dfs(i);
                 answer++;
             }
         }
