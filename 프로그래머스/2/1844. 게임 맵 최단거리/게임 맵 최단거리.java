5b0f4c3d4c80cfcca11907fba491c13510fe9753
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2025-08-30 01:04:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.12 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,50 +1,64 @@
 import java.util.*;
 
 class Point{
-    int x,y,cnt;
-    Point(int x,int y,int cnt){
+    int x,y;
+    
+    Point(int x,int y){
         this.x=x;
         this.y=y;
-        this.cnt=cnt;
     }
 }
 
 class Solution {
     public int solution(int[][] maps) {
-        int answer=0;
-        int n=maps.length;
-        int m=maps[0].length;
-        boolean flag=false;
+        int answer = 0;
+        int n = maps.length;
+        int m = maps[0].length;
+        int cnt = 1;
+        boolean flag = false;
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        boolean[][] visit = new boolean[n][m];
         
-        int[] dx={-1,1,0,0};
-        int[] dy={0,0,-1,1};
-    
-        Queue<Point> q=new LinkedList<>();
+        Queue<Point> q = new LinkedList<>();
         
-        q.offer(new Point(0,0,1));
-        maps[0][0]=0;
+        q.offer(new Point(0,0));
+        visit[0][0] = true;
         
         while(!q.isEmpty()){
-            int size=q.size();
+            int size = q.size();
+            
             for(int i=0;i<size;i++){
-                Point p=q.poll();
+                Point p = q.poll();
+                
                 if(p.x==n-1&&p.y==m-1){
-                    flag=true;
-                    answer=p.cnt;
+                    flag = true;
+                    answer = cnt;
                     break;
                 }
+                
                 for(int j=0;j<4;j++){
-                    int a=p.x+dx[j];
-                    int b=p.y+dy[j];
-                    if(a>=0&&a<n&&b>=0&b<m&&maps[a][b]==1){
-                        q.offer(new Point(a,b,p.cnt+1));
-                        maps[a][b]=0;
+                    int a = p.x+dx[j];
+                    int b = p.y+dy[j];
+                    
+                    if(a>=0&&a<n&&b>=0&&b<m){
+                        if(maps[a][b]==1&&!visit[a][b]){
+                            q.offer(new Point(a,b));
+                            visit[a][b] = true;
+                        }
                     }
                 }
             }
+            
+            if(flag){
+                break;
+            }
+            cnt++;
         }
-        if(!flag)
-            answer=-1;
+        
+        if(!flag){
+            answer = -1;
+        }
         
         return answer;
     }
