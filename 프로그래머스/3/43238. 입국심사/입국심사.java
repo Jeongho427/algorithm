2dbc8cf67c3b887fbed2b99ab34de9974495524a
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2025-08-30 20:26:59 +0900

    [level 3] Title: 입국심사, Time: 104.79 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,26 +1,28 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
+        
         Arrays.sort(times);
         
-        long left = 1; // 최소 시간
-        long right = (long) times[times.length - 1] * n; // 최악의 경우 (가장 오래 걸리는 심사관이 모든 사람을 심사할 경우)
-        long answer = right;
+        long lt = 1;
+        long rt = (long)times[0]*n;
         
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            long sum = 0; // mid 시간 동안 심사할 수 있는 총 사람 수
+        while(lt<=rt){
+            long mid = (lt+rt)/2;
+            long sum = 0;
             
-            for (int time : times) {
-                sum += mid / time; // 각 심사관이 mid 시간 동안 심사할 수 있는 사람 수를 더함
+            for(int t:times){
+                sum += mid/t;
             }
             
-            if (sum >= n) {
+            if(sum>=n){
                 answer = mid;
-                right = mid - 1; // 시간을 줄여서 더 최적의 답을 찾기 위해
-            } else {
-                left = mid + 1; // 심사할 수 있는 사람이 부족하므로 시간을 늘려야 함
+                rt = mid - 1;
+            }
+            else{
+                lt = mid + 1;
             }
         }
         
