81c95587c376876602c703364a0f177ad2fdfe79
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2025-08-30 15:36:15 +0900

    [level 3] Title: 단어 변환, Time: 0.41 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,42 +3,48 @@ import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        int cnt=0;
-        int[] check=new int[words.length];
+        int cnt = 0;
+        boolean flag = false;
+        boolean[] check = new boolean[words.length];
         
-        HashSet<String> hs=new HashSet<>();
-        Queue<String> q=new LinkedList<>();
-        
-        for(String s:words){
-            hs.add(s);
-        }
+        Queue<String> q = new LinkedList<>();
         
         q.offer(begin);
         
         while(!q.isEmpty()){
-            int size=q.size();
+            int size = q.size();
             
             for(int i=0;i<size;i++){
-                String word=q.poll();
+                String word = q.poll();
                 
                 if(word.equals(target)){
-                    answer=cnt;
+                    flag= true;
+                    answer = cnt;
                     break;
                 }
                 
                 for(int j=0;j<words.length;j++){
-                    int diff=0;
-                    for(int k=0;k<word.length();k++){
+                    int diff = 0;
+                    
+                    if(check[j]){
+                        continue;
+                    }
+                    
+                    for(int k=0;k<words[j].length();k++){
                         if(word.charAt(k)!=words[j].charAt(k)){
                             diff++;
                         }
                     }
                     
-                    if(diff==1&&check[j]==0){
+                    if(diff==1){
                         q.offer(words[j]);
-                        check[j]=1;
+                        check[j] = true;
                     }
                 }
+            }
+            
+            if(flag){
+                break;
             }
             cnt++;
         }
