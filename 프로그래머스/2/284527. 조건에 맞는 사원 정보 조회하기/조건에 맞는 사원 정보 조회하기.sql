8b304ab4782b52243b86417c1a149e005bfb199d
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2025-10-10 22:48:39 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,10 +1,20 @@
-with score_sum as (
-select emp_no, sum(score) total_score
-from hr_grade
-group by emp_no
+-- 1) 사번별 2022년 총점 집계
+WITH score_sum AS (
+  SELECT 
+    g.emp_no,
+    SUM(g.score) AS score
+  FROM hr_grade AS g
+  WHERE g.year = 2022
+  GROUP BY g.emp_no
 )
-
-select s.total_score score, e.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
-from hr_employees e join score_sum s on e.emp_no = s.emp_no
-order by s.total_score desc
-limit 1
+-- 2) 최댓값과 같은 사람들만 조회
+SELECT 
+  ss.score AS SCORE,
+  e.emp_no AS EMP_NO,
+  e.emp_name AS EMP_NAME,
+  e.position AS POSITION,
+  e.email AS EMAIL
+FROM score_sum ss
+JOIN hr_employees e ON e.emp_no = ss.emp_no
+WHERE ss.score = (SELECT MAX(score) FROM score_sum)
+ORDER BY ss.score DESC, e.emp_no;
