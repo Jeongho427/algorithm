2f10912380874a13a4119724a0844bf70e4b0e91
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2025-10-16 21:02:32 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,4 @@
+select i.animal_id, i.animal_type, i.name
+from animal_ins i join animal_outs o on i.animal_id = o.animal_id
+where i.SEX_UPON_INTAKE like 'Intact%' and o.SEX_UPON_OUTCOME not like 'Intact%'
+order by i.animal_id
