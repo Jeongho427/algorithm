f629bdcba98cdd039c9b70246f5593bf8cdab3e5
Jeongho427 <60309978+Jeongho427@users.noreply.github.com>
2025-10-17 14:51:59 +0900

    [level 2] Title: 타겟 넘버, Time: 9786.77 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,22 @@
 import java.util.*;
 
 class Solution {
-    int cnt;
-    int size;
+    int n,answer;
     
-    void DFS(int L,int sum,int[] numbers,int target){
-        if(L==size){
-            if(sum==target){
-                cnt++;
-            }
+    void dfs(int L,int start,int sum,int[] numbers,int target){
+        if(L==n&&sum==target){
+            answer++;
         }
-        else{
-            DFS(L+1,sum+numbers[L],numbers,target);
-            DFS(L+1,sum-numbers[L],numbers,target);
+        for(int i=start;i<n;i++){
+            dfs(L+1,i+1,sum-numbers[i],numbers,target);
+            dfs(L+1,i+1,sum+numbers[i],numbers,target);
         }
     }
-        
+    
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        size=numbers.length;
+        n = numbers.length;
         
-        DFS(0,0,numbers,target);
-        
-        answer=cnt;
+        dfs(0,0,0,numbers,target);
         
         return answer;
     }
